7f42398c5817245adf7e2ad4d8a736429380f582
Marian P. Gajda <marianpgajda@gmail.com>
2020-04-03 07:57:27 +0200

    docs theme 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -28,6 +28,7 @@ author = '[mpg] Marian P. Gajda'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	"sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
